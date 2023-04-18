d291c812aac3a9d54cb7f63a8409258ac9f44c63
cholker ajay kumar <cholkerajaykumar@gmail.com>
2023-04-18 07:32:19 -0400

    removed console log in Nopage

--- client/src/pages/NoPage.js
+++ client/src/pages/NoPage.js
@@ -1,14 +1,12 @@
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 function CreateRoom() {
-    console.log("test")
-    return (
-        <>
-            <div className="flex flex-col">
-                <h3>Page Not Found</h3>
-                <Button variant="contained">Go Home</Button>
-
-            </div>
-        </>
-      );
-};
+  return (
+    <>
+      <div className="flex flex-col">
+        <h3>Page Not Found</h3>
+        <Button variant="contained">Go Home</Button>
+      </div>
+    </>
+  );
+}
 export default CreateRoom;
