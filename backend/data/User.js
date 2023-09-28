38ff2e083889d8b3838dddb4b9c1a92074331d00
cholker ajay kumar <cholkerajaykumar@gmail.com>
2023-09-28 13:33:47 -0400

    add user file to backend

---
+++ backend/data/User.js
@@ -0,0 +1,36 @@
+module.exports = {
+  createUser: async function (email, username, password, confirmPassword) {
+    let data = [];
+    data = await new Promise((resolve, reject) => {
+      connection.query(
+        "call simply_online.create_user(?,?,?,?)",
+        [email, username, password, confirmPassword],
+        function (err, result) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(JSON.parse(JSON.stringify(result)));
+          }
+        }
+      );
+    });
+    return data;
+  },
+  verifyUser: async function (email, password) {
+    let data = [];
+    data = await new Promise((resolve, reject) => {
+      connection.query(
+        "call simply_online.verify_user(?,?)",
+        [email, password],
+        function (err, result) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(JSON.parse(JSON.stringify(result)));
+          }
+        }
+      );
+    });
+    return data;
+  },
+};
