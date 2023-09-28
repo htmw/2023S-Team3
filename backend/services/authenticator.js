ac3122dbd607ca61432789b9cc2a429cd7c75aca
cholker ajay kumar <cholkerajaykumar@gmail.com>
2023-09-28 13:35:47 -0400

    add authenticator to backend

---
+++ backend/services/authenticator.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+// Middleware to check JWT authentication
+function authenticator(req, res, next) {
+  let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  try {
+    const token = req.header(tokenHeaderKey);
+    const verified = jwt.verify(token, jwtSecretKey);
+    if (verified) {
+      next();
+    } else {
+      // Access Denied
+      return res.status(401).send(error);
+    }
+  } catch (error) {
+    // Access Denied
+    return res.status(401).send(error);
+  }
+}
+
+module.exports = authenticator;
